8252ecf2a1757b1432938a52eae8f7011df7bf95
David R. Smith <David.Smith@osi.ca.gov>
2019-08-12 15:27:41 -0700

    CMO-99: user trace log

--- api-core-common/src/main/java/gov/ca/cwds/tracelog/CaresTraceLogService.java
+++ api-core-common/src/main/java/gov/ca/cwds/tracelog/CaresTraceLogService.java
@@ -7,8 +7,22 @@ package gov.ca.cwds.tracelog;
  */
 public interface CaresTraceLogService {
 
+  /**
+   * Log search query terms.
+   * 
+   * @param userId user conducting the search
+   * @param term search term
+   * @param value term value
+   */
   void logSearchQuery(String userId, String term, String value);
 
-  void logRecordAccess(String userId, String id);
+  /**
+   * Log access to a records, such as a Client.
+   * 
+   * @param userId user accessing the record
+   * @param id primary key
+   * @param type record type
+   */
+  void logRecordAccess(String userId, String id, String type);
 
 }
