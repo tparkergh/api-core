bdee93417ecfda7c6baa3ac8f8278990704153b4
David R. Smith <David.Smith@osi.ca.gov>
2019-08-13 14:23:38 -0700

    CMO-99: user trace log

---
+++ api-core-common/src/test/java/gov/ca/cwds/tracelog/TestTraceLogRecordAccessDao.java
@@ -0,0 +1,12 @@
+package gov.ca.cwds.tracelog;
+
+import java.util.Date;
+
+public class TestTraceLogRecordAccessDao implements TraceLogRecordAccessDao {
+
+  @Override
+  public void logRecordAccess(String userId, Date moment, String id) {
+
+  }
+
+}
---
+++ api-core-common/src/test/java/gov/ca/cwds/tracelog/TestTraceLogSearchQueryDao.java
@@ -0,0 +1,10 @@
+package gov.ca.cwds.tracelog;
+
+import java.util.Date;
+
+public class TestTraceLogSearchQueryDao implements TraceLogSearchQueryDao {
+
+  @Override
+  public void logSearchQuery(String userId, Date moment, String term, String value) {}
+
+}
