447dca42955a4614596fb53caf3cd3dc21437e8c
David R. Smith <David.Smith@osi.ca.gov>
2019-08-13 13:11:07 -0700

    CMO-99: user trace log

---
+++ api-core-common/src/main/java/gov/ca/cwds/tracelog/TraceLogServiceAsyncImpl.java
@@ -0,0 +1,121 @@
+package gov.ca.cwds.tracelog;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.google.inject.Inject;
+
+import gov.ca.cwds.data.std.ApiObjectIdentity;
+
+public class TraceLogServiceAsyncImpl implements TraceLogService {
+
+  protected static abstract class TraceLogEntry extends ApiObjectIdentity {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String userId;
+    private final Date moment = new Date();
+
+    public TraceLogEntry(String userId) {
+      this.userId = userId;
+    }
+
+    public String getUserId() {
+      return userId;
+    }
+
+    public Date getMoment() {
+      return moment;
+    }
+
+  }
+
+  protected static final class TraceLogQueryEntry extends TraceLogEntry {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String term;
+    private final String value;
+
+    public TraceLogQueryEntry(String userId, String term, String value) {
+      super(userId);
+      this.term = term;
+      this.value = value;
+    }
+
+    public String getTerm() {
+      return term;
+    }
+
+    public String getValue() {
+      return value;
+    }
+
+  }
+
+  protected static final class TraceLogAccessEntry extends TraceLogEntry {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String id;
+    private final String type;
+
+    public TraceLogAccessEntry(String userId, String id, String type) {
+      super(userId);
+      this.id = id;
+      this.type = type;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+  }
+
+  private final TraceLogSearchQueryDao queryDao;
+
+  private final TraceLogRecordAccessDao accessDao;
+
+  protected Queue<TraceLogAccessEntry> accessQueue = new ConcurrentLinkedQueue<>();
+
+  protected Timer accessTimer;
+
+  /**
+   * Trace access to tables under watch, not every table in legacy.
+   */
+  private final List<TraceLogFilter> filters;
+
+  @Inject
+  public TraceLogServiceAsyncImpl(TraceLogSearchQueryDao queryDao, TraceLogRecordAccessDao accessDao,
+      Collection<TraceLogFilter> filters) {
+    this.queryDao = queryDao;
+    this.accessDao = accessDao;
+    this.filters = new ArrayList<TraceLogFilter>(filters);
+  }
+
+  @Override
+  public void logSearchQuery(String userId, String json) {
+    for (Map.Entry<CaresSearchQueryParser.CaresJsonField, String> e : new CaresSearchQueryParser()
+        .parse(json).entrySet()) {
+      queryDao.logSearchQuery(userId, e.getKey().getName(), e.getValue());
+    }
+  }
+
+  @Override
+  public void logRecordAccess(String userId, Object entity, String id) {
+    if (filters.stream().anyMatch(f -> f.traceAccess(userId, entity, id))) {
+      accessDao.logRecordAccess(userId, entity, id);
+    }
+  }
+
+}
